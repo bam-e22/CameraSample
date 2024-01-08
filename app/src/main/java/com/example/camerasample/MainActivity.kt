50072c43d44471560cf5df8521821ec23212d077
bam-e22 <stack.07142@gmail.com>
2024-01-08 22:07:51 +0900

    HomeViewModel 생성

--- app/src/main/java/com/example/camerasample/MainActivity.kt
+++ app/src/main/java/com/example/camerasample/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.camerasample
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.camerasample.ui.screen.Camera2Screen
 import com.example.camerasample.ui.screen.HomeScreen
+import com.example.camerasample.ui.screen.HomeViewModel
 import com.example.camerasample.ui.theme.CameraSampleTheme
 import com.example.camerax.basic.CameraXBasicScreen
 import com.example.camerax.basic.CameraXBasicViewModel
@@ -24,7 +25,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
-            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+            navigationBarStyle = SystemBarStyle.auto(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
         )
         setContent {
             CameraSampleTheme {
@@ -41,6 +45,7 @@ class MainActivity : ComponentActivity() {
                             navigateToCamera2 = {
                                 navController.navigate("camera2")
                             },
+                            viewModel = hiltViewModel<HomeViewModel>()
                         )
                     }
                     composable("camerax") {
@@ -51,10 +56,12 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                     composable("camera2") {
-                        Camera2Screen(
-                            navigateUp = navController::navigateUp,
-                            modifier = Modifier.fillMaxSize()
-                        )
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            color = Color.Blue
+                        ) {
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/camerasample/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/camerasample/ui/screen/HomeScreen.kt
@@ -6,42 +6,81 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.camerasample.ui.components.Description
 import com.example.camerasample.ui.components.NavButton
+import com.example.camerasample.ui.components.Title
 
 @Composable
 fun HomeScreen(
     navigateToCameraX: () -> Unit,
     navigateToCamera2: () -> Unit,
+    viewModel: HomeViewModel,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        viewModel.checkConcurrentCameraFeature(context)
+    }
     Scaffold(
         modifier = modifier
     ) { paddingValues ->
-        Column(
-            modifier = Modifier
+        val concurrentCameraInfo by viewModel.concurrentCameraInfo.collectAsStateWithLifecycle()
+        Contents(
+            navigateToCameraX = navigateToCameraX,
+            navigateToCamera2 = navigateToCamera2,
+            concurrentCameraInfo = concurrentCameraInfo,
+            modifier = modifier
                 .fillMaxSize()
                 .background(Color.LightGray)
-                .padding(
-                    top = paddingValues.calculateTopPadding() + 24.dp,
-                    bottom = paddingValues.calculateBottomPadding(),
-                    start = 24.dp,
-                    end = 24.dp
-                )
-        ) {
-            NavButton(
-                text = "CameraX",
-                onClick = navigateToCameraX
-            )
-            Spacer(Modifier.height(20.dp))
-            NavButton(
-                text = "Camera2",
-                onClick = navigateToCamera2
-            )
-        }
+                .padding(paddingValues)
+        )
+    }
+}
+
+@Composable
+private fun Contents(
+    navigateToCameraX: () -> Unit,
+    navigateToCamera2: () -> Unit,
+    concurrentCameraInfo: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(24.dp)
+    ) {
+        // Samples
+        Title(
+            text = "Samples"
+        )
+        Spacer(Modifier.height(20.dp))
+        NavButton(
+            text = "CameraX Basic Sample",
+            onClick = navigateToCameraX
+        )
+        Spacer(Modifier.height(10.dp))
+        NavButton(
+            text = "Camera2 Basic Sample",
+            onClick = navigateToCamera2
+        )
+        Spacer(Modifier.height(20.dp))
+        Divider()
+        Spacer(Modifier.height(20.dp))
+        // ConcurrentCamera Feature check
+        Title(
+            text = "Concurrent Camera Feature"
+        )
+        Description(
+            text = "TODO: Not implemented yet"
+        )
     }
 }
